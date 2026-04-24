f8d5e2c599bbbc1b696bbc75af7777fde4b42b93
gopinath <gopinath.vmurugan.gmail.com>
2026-04-24 12:03:15 +0530

    update connection

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -26,7 +26,10 @@ builder.Services.AddAuthentication(options =>
 /*builder.Services.AddDbContext<AuthDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));*/
 builder.Services.AddDbContext<AuthDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        o => o.EnableRetryOnFailure()
+    ));
 
 // Add services to the container.
 
